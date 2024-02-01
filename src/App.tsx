f3bf89fa037bae77454a04ff472eace6414520e3
Rory Wood <rorywood9@live.com>
2024-01-31 19:17:43 -0500

    added files from local

---
+++ src/App.tsx
@@ -0,0 +1,111 @@
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react"
+import { InputSelect } from "./components/InputSelect"
+import { Instructions } from "./components/Instructions"
+import { Transactions } from "./components/Transactions"
+import { useEmployees } from "./hooks/useEmployees"
+import { usePaginatedTransactions } from "./hooks/usePaginatedTransactions"
+import { useTransactionsByEmployee } from "./hooks/useTransactionsByEmployee"
+import { EMPTY_EMPLOYEE } from "./utils/constants"
+import { Employee } from "./utils/types"
+
+export function App() {
+  const { data: employees, ...employeeUtils } = useEmployees()
+  const { data: paginatedTransactions, ...paginatedTransactionsUtils } = usePaginatedTransactions()
+  const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
+  const [isLoading, setIsLoading] = useState(false)
+  // Creates state to determine if view more button should be shown
+  const [viewMore, setViewMore] = useState(true)
+
+  const transactions = useMemo(
+    () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
+    [paginatedTransactions, transactionsByEmployee]
+  )
+
+  const loadAllTransactions = useCallback(async () => {
+    setIsLoading(true)
+    transactionsByEmployeeUtils.invalidateData()
+
+    await employeeUtils.fetchAll()
+    // Ensures Loading employees stops beingn shown when request for employees is complete.
+    setIsLoading(false)
+    await paginatedTransactionsUtils.fetchAll()
+
+  }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
+
+  const loadTransactionsByEmployee = useCallback(
+    async (employeeId: string) => {
+      paginatedTransactionsUtils.invalidateData()
+      await transactionsByEmployeeUtils.fetchById(employeeId)
+      // If filtering by employee, set viewMore to false
+      setViewMore(false)
+    },
+    [paginatedTransactionsUtils, transactionsByEmployeeUtils]
+  )
+
+  // If no more data to be shown, set viewMore to false
+  useEffect(() => {
+    if (paginatedTransactions?.nextPage === null && !!paginatedTransactions?.data) {
+      setViewMore(false)
+    }
+  }, [paginatedTransactions])
+
+  useEffect(() => {
+    if (employees === null && !employeeUtils.loading) {
+      loadAllTransactions()
+    }
+  }, [employeeUtils.loading, employees, loadAllTransactions])
+
+  return (
+    <Fragment>
+      <main className="MainContainer">
+        <Instructions />
+
+        <hr className="RampBreak--l" />
+
+        <InputSelect<Employee>
+          isLoading={isLoading}
+          defaultValue={EMPTY_EMPLOYEE}
+          items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
+          label="Filter by employee"
+          loadingLabel="Loading employees"
+          parseItem={(item) => ({
+            value: item.id,
+            label: `${item.firstName} ${item.lastName}`,
+          })}
+          onChange={async (newValue) => {
+            if (newValue === null) {
+              return
+            }
+            // Checks if no id assigned and if not loads all transactions
+            if (newValue.id == '') {
+              await loadAllTransactions()
+              // If filtering by all employees, set viewMore to true
+              setViewMore(true)
+            } else {
+              await loadTransactionsByEmployee(newValue.id)
+            }
+          }}
+        />
+
+        <div className="RampBreak--l" />
+
+        <div className="RampGrid">
+          <Transactions transactions={transactions} />
+
+          {transactions !== null && viewMore && (
+            // Only show view more button if state is true
+            <button
+              className="RampButton"
+              disabled={paginatedTransactionsUtils.loading}
+              onClick={async () => {
+                await loadAllTransactions()
+              }}
+            >
+              View More
+            </button>
+          )}
+        </div>
+      </main>
+    </Fragment>
+  )
+}
---
+++ src/hooks/usePaginatedTransactions.ts
@@ -0,0 +1,38 @@
+import { useCallback, useState } from "react"
+import { PaginatedRequestParams, PaginatedResponse, Transaction } from "../utils/types"
+import { PaginatedTransactionsResult } from "./types"
+import { useCustomFetch } from "./useCustomFetch"
+
+export function usePaginatedTransactions(): PaginatedTransactionsResult {
+  const { fetchWithCache, loading } = useCustomFetch()
+  const [paginatedTransactions, setPaginatedTransactions] = useState<PaginatedResponse<
+    Transaction[]
+  > | null>(null)
+
+  const fetchAll = useCallback(async () => {
+    const response = await fetchWithCache<PaginatedResponse<Transaction[]>, PaginatedRequestParams>(
+      "paginatedTransactions",
+      {
+        page: paginatedTransactions === null ? 0 : paginatedTransactions.nextPage,
+      }
+    )
+
+    setPaginatedTransactions((previousResponse) => {
+      if (response === null || previousResponse === null) {
+        return response
+      }
+
+      // Concantenated previous transactions loaded with newly fetched transactions
+      const moreTransations = paginatedTransactions?.data || []
+      const allTransactions = [...moreTransations, ...response.data]
+
+      return { data: allTransactions, nextPage: response.nextPage };
+    })
+  }, [fetchWithCache, paginatedTransactions])
+
+  const invalidateData = useCallback(() => {
+    setPaginatedTransactions(null)
+  }, [])
+
+  return { data: paginatedTransactions, loading, fetchAll, invalidateData }
+}
